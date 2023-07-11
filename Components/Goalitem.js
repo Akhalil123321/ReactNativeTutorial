f12de65850bfee2eb0bc53562dbeb95145206979
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-07-12 01:32:35 +0400

    .

--- Components/Goalitem.js
+++ Components/Goalitem.js
@@ -1,7 +1,7 @@
 import { StyleSheet, View, Text , Pressable, TouchableOpacity} from "react-native"
 export default function GoalItem(props) {
   return (
-		<Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+		<Pressable onLongPress={props.onDeleteItem.bind(this, props.id)}>
 			<View style={styles.goalItem}>
 				<Text style={styles.goalText}>{props.text}</Text>
 			</View>
